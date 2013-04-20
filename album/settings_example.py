5047c62029a94e523ddf0b89599b169c8fcec314
Hyungsuk Yoon <youhanmir@gmail.com>
2013-04-20 23:39:05 +0900

    - update my gitignore - get jqeruy.js - update settings

--- album/settings_example.py
+++ album/settings_example.py
@@ -1,6 +1,8 @@
 # You should copy this file to settings.py and setting!
 # Django settings for album project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -62,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), '../static_collect/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -70,6 +72,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+	os.path.join(os.path.dirname(__file__), '../static/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -109,6 +112,7 @@ ROOT_URLCONF = 'album.urls'
 WSGI_APPLICATION = 'album.wsgi.application'
 
 TEMPLATE_DIRS = (
+	os.path.join(os.path.dirname(__file__), '../templates/'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
